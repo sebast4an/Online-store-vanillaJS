b7b0ea8819ee60b4d2d15ea6c65ed4d07880480f
sebast4an <szczypinskisebastian@gmail.com>
2021-06-13 18:59:49 +0200

    I add draft figma project and build structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 import "../scss/main.scss";
 import "../img/file.png";
 
-const p = document.querySelector("p");
-p.innerText = "working!!";
-
-const oneOne = document.querySelector(".super1");
-oneOne.innerText = "red?";
