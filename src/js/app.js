b28b2865978baed3524de212769ce366b5f96da7
sebast4an <szczypinskisebastian@gmail.com>
2021-06-25 21:54:01 +0200

    API data handling

--- src/js/app.js
+++ src/js/app.js
@@ -9,9 +9,7 @@ const getDataFromAPI = async question => {
     try {
         const response = await fetch(URL);
         const data = await response.json();
-
         return data;
-
     } catch (error) {
         console.log(error)
     };
--- src/js/products.js
+++ src/js/products.js
@@ -1,17 +1,42 @@
 import {getDataFromAPI, randomNumber} from "./app";
 
+const recommendProducts = document.querySelector(".recommend__products");
 
-const makeRecommendProducts = () => {
-    const productsContainer = document.querySelector(".recommend__products");
+const addProductsToHTML = (when, howMuchToAdd) => {
+    //TODO: The same template for recommend and bestseller product?!
     const template = document.querySelector(".recommend__template");
     const clone = template.content.cloneNode(true);
     
-    const image = clone.querySelector(".image__img");
+    const image = clone.querySelector(".product__image");
     const name = clone.querySelector(".product__name");
     const price = clone.querySelector(".product__price");
-    
+
+
     getDataFromAPI("products").then(data => {
+        let productKeysToAdd = [];
         const sumOfProducts = data.length;
+
+        const generatingNumbers = (start, howMuchToAdd) => {
+
+            for (let i=start; i<howMuchToAdd; i++) {
+                let number = randomNumber(0, sumOfProducts);            
+                productKeysToAdd.push(number);
+             }; 
+
+             const checkDuplicate = new Set(productKeysToAdd);
+             productKeysToAdd = [...checkDuplicate];
+             console.log(productKeysToAdd);
+        }; generatingNumbers(0, howMuchToAdd);
+        
+        if (productKeysToAdd.length < howMuchToAdd) {
+            generatingNumbers(productKeysToAdd.length, howMuchToAdd);
+            console.log(productKeysToAdd);
+        };
+
+
+
+
+
         let number = randomNumber(0, sumOfProducts);
         
         console.log(data[number]);
@@ -20,6 +45,8 @@ const makeRecommendProducts = () => {
         price.innerText = data[number].price;
     });
 
-    productsContainer.append(clone);
+    when.append(clone);
 
-}; makeRecommendProducts();
+}; 
+
+addProductsToHTML(recommendProducts, 6);
