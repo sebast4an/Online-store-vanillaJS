fb1ac787f24ee406b976fd8fada6a69a90da06d2
sebast4an <szczypinskisebastian@gmail.com>
2021-07-02 20:39:34 +0200

    product cart

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/main.scss";
 import "../assets/icons/search.svg";
 import "../assets/icons/user.svg";
 import "../assets/icons/basket.svg";
+import "../assets/icons/add-basket.svg"
 
 import {renderRandomProducts} from "./products";
 
--- src/js/products.js
+++ src/js/products.js
@@ -19,13 +19,15 @@ const renderRandomProducts = (data, where, howManyToAdd) => {
                 <figure class="product__border">
                     <img class="product__img" src="${data[key]["image"]}">
                 </figure>
-                <p class="product__name">${data[key]["title"]}</p>
-                <section class="product__price price">
-                    <p class="price__price">${data[key]["price"]} $</p>
-                    <button type="button" class="price__button">
-                    Add to cart
-                    </button>
-                </section>
+                <header class="product__header" >
+                    <h3 class="product__name">${data[key]["title"]}</h3>
+                    <section class="product__price price">
+                        <p class="price__price">${data[key]["price"]} $</p>
+                        <button type="button" class="price__button">
+                            <img class="price__button--img" src="/assets/add-basket.svg">
+                        </button>
+                    </section>
+                </header>
         `;
         fragment.appendChild(product);
     }; 
