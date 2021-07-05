4adaa6f6119cfc20b69bb08e6bb48146d201a7f6
sebast4an <szczypinskisebastian@gmail.com>
2021-07-05 23:06:00 +0200

    connect comments restAPI

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,27 +1,32 @@
 import {randomNumber, randomContent} from "./products";
 
+const renderComments = (howManyToRender) => {
+    const comments = document.querySelector(".comments__post");
+    const fragment = document.createDocumentFragment();
+    const commentsNumber = randomContent(4, data.length-1);    
+
+    const appendComments = () => {
+        const comment = document.createElement("div");
+        comment.classList.add(`comments__element`);
+        comment.classList.add(`comments__element--${ }`); 
+                                                //TODO: Add data element.
+    }
+
+};
+
 const getCommentsFromAPI = async () => {
     const URL = `https://jsonplaceholder.typicode.com/comments`;
 
     try {
         const response = await fetch(URL);
         const data = await response.json();
-        const comments = document.querySelector(".comments__post");
+
         console.log(data);
 
     } catch (error) {
         console.log(error);
     }
 };
-
-const renderComments = (howManyToRender) => {
-    const fragment = document.createDocumentFragment();
-    const commentsNumber = randomContent(4, data.length-1);
-
-    
-    console.log(commentsNumber);
-
-}
 
 
 
