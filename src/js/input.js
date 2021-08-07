417da6a0382b685b65d814409bfb689b8fcf99cc
sebast4an <szczypinskisebastian@gmail.com>
2021-08-07 17:16:35 +0200

    modal / dialog for search

--- src/js/input.js
+++ src/js/input.js
@@ -2,12 +2,25 @@ window.addEventListener('DOMContentLoaded', () => {
   //search input
   const openModalButton = document.querySelector('.modal-search--open');
   const modal = document.querySelector('.modal-search');
+  const modalCloseButton = document.querySelector('.modal-search--close');
   const modalBehind = document.createElement('div');
   modalBehind.classList.add('modal-search__behind');
 
   openModalButton.addEventListener('click', () => {
     modal.after(modalBehind);
+    modalCloseButton.style.display = 'block';
     modal.showModal();
+  });
+
+  modalCloseButton.addEventListener('click', () => {
+    modal.close();
+  });
+
+  //I adding event listener for close because
+  //user posibli close modal by type esc button on keyboard
+  modal.addEventListener('close', () => {
+    modalCloseButton.style.display = '';
+    modalBehind.remove();
   });
 
   //newsletter email input
