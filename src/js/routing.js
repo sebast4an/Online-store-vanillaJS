d40c4f87f7f6fcefa3a49518401af2e5d5f086f1
sebast4an <szczypinskisebastian@gmail.com>
2021-08-13 21:07:57 +0200

    backup

--- src/js/routing.js
+++ src/js/routing.js
@@ -4,68 +4,71 @@ import { loginComponent, loginLoader } from '../components/loginPanel';
 import { basketComponent, basketLoader } from '../components/basket';
 import { productPageComponent, productPageLoader } from '../components/productPage';
 
-const content = document.querySelector('.content');
+(() => {
+  const content = document.querySelector('.content');
 
-const routes = {
-  '/': homeComponent,
-  '/basket': basketComponent,
-  '/login-panel': loginComponent,
-  '/electronics': categoryProductsComponent,
-  '/jewelery': categoryProductsComponent,
-  '/men%27s%20clothing': categoryProductsComponent,
-  '/women%27s%20clothing': categoryProductsComponent,
-  '/product-id': productPageComponent,
-};
+  const routes = {
+    '/': homeComponent,
+    '/basket': basketComponent,
+    '/login-panel': loginComponent,
+    '/electronics': categoryProductsComponent,
+    '/jewelery': categoryProductsComponent,
+    '/men%27s%20clothing': categoryProductsComponent,
+    '/women%27s%20clothing': categoryProductsComponent,
+    '/product-id': productPageComponent,
+  };
 
-const loaderComponents = pathname => {
-  window.history.pushState({}, pathname, window.location.origin + pathname);
+  const loaderComponents = pathname => {
+    window.history.pushState({}, pathname, window.location.origin + pathname);
 
-  if (pathname.slice(1, 11) == 'product-id') {
-    document.title = `Online Store - ${pathname.slice(1)}`;
-    content.innerHTML = routes['/product-id'];
-    productPageLoader(pathname.slice(12));
-  } else {
-    switch (pathname) {
-      case '/login-panel':
-        document.title = `Online Store - Sign up or Sign in`;
-        content.innerHTML = routes[pathname];
-        loginLoader();
-        break;
-      case '/basket':
-        document.title = `Online Store - ${pathname.slice(1)}`;
-        content.innerHTML = routes[pathname];
-        basketLoader();
-        break;
-      case '/electronics':
-        document.title = `Online Store - ${pathname.slice(1)}`;
-        content.innerHTML = routes[pathname];
-        categoryProductsLoader('electronics');
-        break;
-      case '/jewelery':
-        document.title = `Online Store - ${pathname.slice(1)}`;
-        content.innerHTML = routes[pathname];
-        categoryProductsLoader('jewelery');
-        break;
-      case '/men%27s%20clothing':
-        document.title = `Online Store - ${decodeURI(pathname.slice(1))}`;
-        content.innerHTML = routes[pathname];
-        categoryProductsLoader('men%27s%20clothing');
-        break;
-      case '/women%27s%20clothing':
-        document.title = `Online Store - ${decodeURI(pathname.slice(1))}`;
-        content.innerHTML = routes[pathname];
-        categoryProductsLoader('women%27s%20clothing');
-        break;
-      default:
-        document.title = 'Online Store';
-        content.innerHTML = routes['/'];
-        homeLoader();
+    if (pathname.slice(1, 11) == 'product-id') {
+      document.title = `Online Store - ${pathname.slice(1)}`;
+      content.innerHTML = routes['/product-id'];
+      productPageLoader(pathname.slice(12));
+    } else {
+      switch (pathname) {
+        case '/login-panel':
+          document.title = `Online Store - Sign up or Sign in`;
+          content.innerHTML = routes[pathname];
+          loginLoader();
+          break;
+        case '/basket':
+          document.title = `Online Store - ${pathname.slice(1)}`;
+          content.innerHTML = routes[pathname];
+          basketLoader();
+          break;
+        case '/electronics':
+          document.title = `Online Store - ${pathname.slice(1)}`;
+          content.innerHTML = routes[pathname];
+          categoryProductsLoader('electronics');
+          break;
+        case '/jewelery':
+          document.title = `Online Store - ${pathname.slice(1)}`;
+          content.innerHTML = routes[pathname];
+          categoryProductsLoader('jewelery');
+          break;
+        case '/men%27s%20clothing':
+          document.title = `Online Store - ${decodeURI(pathname.slice(1))}`;
+          content.innerHTML = routes[pathname];
+          categoryProductsLoader('men%27s%20clothing');
+          break;
+        case '/women%27s%20clothing':
+          document.title = `Online Store - ${decodeURI(pathname.slice(1))}`;
+          content.innerHTML = routes[pathname];
+          categoryProductsLoader('women%27s%20clothing');
+          break;
+        default:
+          document.title = 'Online Store';
+          content.innerHTML = routes['/'];
+          homeLoader();
+      }
     }
-  }
-  disableRefreshForAllLinks();
-};
+  };
 
-const disableRefreshForAllLinks = () => {
+  //first website loading
+  content.innerHTML = routes[window.location.pathname];
+  loaderComponents(window.location.pathname);
+
   const allLinks = document.querySelectorAll('a');
   allLinks.forEach(link => {
     link.addEventListener('click', e => {
@@ -78,13 +81,6 @@ const disableRefreshForAllLinks = () => {
       }
     });
   });
-};
-
-(() => {
-  //first website loading
-  content.innerHTML = routes[window.location.pathname];
-  loaderComponents(window.location.pathname);
-  disableRefreshForAllLinks();
 
   window.addEventListener('popstate', () => {
     loaderComponents(window.location.pathname);
