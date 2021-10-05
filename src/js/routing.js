920ede2923dfccd1c25029895ae8c9adbda654d4
sebast4an <szczypinskisebastian@gmail.com>
2021-10-05 20:56:03 +0200

    routing

--- src/js/routing.js
+++ src/js/routing.js
@@ -19,7 +19,7 @@ import { productPageComponent, productPageLoader } from '../components/productPa
   };
 
   const loaderComponents = pathname => {
-    window.history.pushState({}, pathname, window.location.origin + '/online-store-vanillaJS' + pathname);
+    window.history.pushState({}, pathname, window.location.origin + pathname);
 
     if (pathname.slice(1, 11) == 'product-id') {
       document.title = `Online Store - ${pathname.slice(1)}`;
@@ -83,6 +83,6 @@ import { productPageComponent, productPageLoader } from '../components/productPa
   });
 
   window.addEventListener('popstate', () => {
-    loaderComponents(window.location);
+    loaderComponents(window.location.pathname);
   });
 })();
